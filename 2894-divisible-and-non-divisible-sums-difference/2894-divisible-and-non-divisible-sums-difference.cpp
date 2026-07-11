a0bc5da31ec78b3f02bc1b362a7f674efa45d782
Aayush Tongale <tongaleaayush@gmail.com>
2026-07-11 23:07:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (65.87%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -5,7 +5,7 @@ public:
         int num2 =0;
         for(int i = 1; i<=n; i++){
             if(i%m !=0) num1+=i;
-            if(i%m ==0) num2+=i;
+            else num2+=i;
         }
         return num1-num2;
     }

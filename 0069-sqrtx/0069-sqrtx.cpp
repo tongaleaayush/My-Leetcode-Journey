4dacf2ba76f62d3b60aedf5a4f13f7dac4fecf54
Aayush Tongale <tongaleaayush@gmail.com>
2026-07-14 23:33:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.17%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long low = 0;
+        long long high = x;
+        long long ans;
+        while(low<=high){
+            long long mid = (low+high)/2;
+            long long check = mid*mid;
+            if(check==x){
+                return mid;
+            }
+            if(check<x){
+                ans=mid;
+                low= mid+1;
+            }
+            else high= mid-1;
+
+        }
+        return ans;
     }
 };

9b552c90b79f038fd8dd5569885383faaa30d241
Aayush Tongale <tongaleaayush@gmail.com>
2026-09-18 23:43:26 +0530

    Time: 9 ms (65.39%), Space: 88.9 MB (67.18%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -5,30 +5,29 @@ public:
         int ans = INT_MAX;
         int left = 0;
         int sum = 0;
-        
+
         vector<int> best(n, ans);
 
-        for(int right=0; right<n; right++){
-              sum += arr[right];    
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
 
-             while(sum>target){
-                sum-=arr[left];
+            while (sum > target) {
+                sum -= arr[left];
                 left++;
-                }
+            }
 
-                if(right > 0){
-        best[right] = best[right - 1];
-    }
-            if(sum==target){
-                int currlength = right-left+1;
-                
-                if(left>0 && best[left-1] != INT_MAX){
-                    ans = min(ans, best[left-1]+ currlength);
+            if (right > 0) {
+                best[right] = best[right - 1];
+            }
+            if (sum == target) {
+                int currlength = right - left + 1;
+
+                if (left > 0 && best[left - 1] != INT_MAX) {
+                    ans = min(ans, best[left - 1] + currlength);
                 }
 
                 best[right] = min(best[right], currlength);
             }
-          
         }
         return ans == INT_MAX ? -1 : ans;
     }
